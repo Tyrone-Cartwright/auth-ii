ff5f7d9f04ebc038b8ab1b991bc6226845bbe0dc
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-01-23 18:06:43 -0500

    imported Link/Route in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
+import { Link, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Authorized Personnel Only</h1>
       </div>
     );
   }
