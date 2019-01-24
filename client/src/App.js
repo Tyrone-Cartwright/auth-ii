c6e2680ec48fead04b15e47a7f21a8cb5d7a2e89
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-01-24 18:41:01 -0500

    Added users register and auth files

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,39 @@
 import React, { Component } from "react";
+import { Route, NavLink } from "react-router-dom";
+
 import "./App.css";
-//import { Link, Route } from "react-router-dom";
+
+import Users from "./users/Users";
+import Signin from "./auth/Signin";
+import Register from "./register/registerForm";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Authorized Personnel Only</h1>
-      </div>
+      <>
+        <header>
+          <nav>
+            <NavLink to="/signin">Signin</NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/users">Users</NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/register">Register</NavLink>
+            <button onClick={this.signout}>Signout</button>
+          </nav>
+        </header>
+        <main>
+          <Route path="/signin" component={Signin} />
+          <Route path="/users" component={Users} />
+          <Route path="/register" component={Register} />
+        </main>
+      </>
     );
   }
+
+  signout = () => {
+    localStorage.removeItem("jwt");
+    window.location.replace("/signin");
+  };
 }
 
 export default App;
